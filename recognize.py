a2d2964be5dd2aa96612382e761a888daa223ff4
Евгений Шевченко <45338087+3530385@users.noreply.github.com>
2023-02-14 10:41:25 +0300

    Add files via upload

---
+++ recognize.py
@@ -0,0 +1,59 @@
+import os
+import glob
+import argparse
+import numpy as np
+import PIL.Image as Image
+from sklearn import preprocessing
+
+import torch
+import torchvision.transforms as transforms
+
+from nn import CaptchaModel
+from util import decode_predictions
+
+
+def recognize(opt):
+    image_files = glob.glob(os.path.join(opt.data_path, "*.png"))
+    targets_orig = [x.split("/")[-1][:-4] for x in image_files]
+    targets = [[c for c in x] for x in targets_orig]
+    targets_flat = [c for clist in targets for c in clist]
+
+    lbl_enc = preprocessing.LabelEncoder()
+    lbl_enc.fit(targets_flat)
+
+    image = Image.open(os.path.join(opt.data_path, opt.image_path)).convert("RGB")
+    image = image.resize(
+        (opt.width, opt.height), resample=Image.BILINEAR
+    )
+    image = np.array(image)
+    mean = (0.485, 0.456, 0.406)
+    std = (0.229, 0.224, 0.225)
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(mean, std, inplace=True)
+    ])
+
+    image = transform(image)
+    image = image.unsqueeze(0)
+
+    model = CaptchaModel(num_chars=len(lbl_enc.classes_))
+    model.load_state_dict(torch.load(opt.saved_model))
+
+    with torch.no_grad():
+        model.eval()
+        pred, _ = model(image)
+        current_preds = decode_predictions(pred, lbl_enc)
+        print(f'Ground truth: {opt.image_path[:-4]}\n'
+              f'Prediction: {current_preds[0]}')
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Captcha recognition using RNN')
+    parser.add_argument('--data_path', type=str, default='samples', help='path to data folder')
+    parser.add_argument('--saved_model', type=str, default='weights/final.pth', help='path to saved model')
+    parser.add_argument('--height', type=int, default=50, help='height of the input image')
+    parser.add_argument('--width', type=int, default=200, help='width of the input image')
+    parser.add_argument('--image_path', type=str, default='n8pfe.png', help='path to test image')
+    opt = parser.parse_args()
+
+    recognize(opt)
---
+++ train.py
@@ -0,0 +1,104 @@
+import os
+import glob
+import tqdm
+import argparse
+import numpy as np
+
+import torch
+
+from sklearn import preprocessing
+from sklearn import model_selection
+
+from dataset import CaptchaDataset
+from nn import CaptchaModel
+from util import weights_init
+
+
+def run_training(opt):
+    image_files = glob.glob(os.path.join(opt.data, "*.png"))
+    targets_orig = [x.split("/")[-1][:-4] for x in image_files]
+    targets = [[c for c in x] for x in targets_orig]
+    targets_flat = [c for clist in targets for c in clist]
+
+    lbl_enc = preprocessing.LabelEncoder()
+    lbl_enc.fit(targets_flat)
+    targets_enc = [lbl_enc.transform(x) for x in targets]
+    targets_enc = np.array(targets_enc)
+    targets_enc = targets_enc + 1
+
+    X_train, X_test, y_train, y_test, _, y_test_orig = model_selection.train_test_split(
+        image_files, targets_enc, targets_orig, test_size=0.1, random_state=42)
+
+    train_dataset = CaptchaDataset(image_paths=X_train, targets=y_train, resize=(opt.height, opt.width))
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=opt.batch_size, num_workers=opt.num_workers,
+                                               shuffle=True)
+    test_dataset = CaptchaDataset(image_paths=X_test, targets=y_test, resize=(opt.height, opt.width))
+    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=opt.batch_size // 2, num_workers=opt.num_workers,
+                                              shuffle=False)
+
+    model = CaptchaModel(num_chars=len(lbl_enc.classes_))
+    model.apply(weights_init)
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    if opt.cuda:
+        model.to(device)
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=3e-4)
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, factor=0.8, patience=5, verbose=True)
+    for epoch in range(opt.num_epochs):
+        model.train()
+        total_loss = 0
+        print(('\n' + '%10s' * 3) % ('epoch', 'loss', 'gpu'))
+        progress_bar = tqdm.tqdm(enumerate(train_loader), total=len(train_loader))
+        for i, (images, targets) in progress_bar:
+            images = images.to(device)
+            targets = targets.to(device)
+            optimizer.zero_grad()
+            _, loss = model(images, targets)
+            loss.backward()
+            optimizer.step()
+            total_loss += loss.detach().cpu().item()
+
+            mem = '%.3gG' % (torch.cuda.memory_reserved() / 1E9 if torch.cuda.is_available() else 0)
+            s = ('%10s' + '%10.4g' + '%10s') % ('%g/%g' % (epoch + 1, opt.num_epochs), total_loss / (i + 1), mem)
+            progress_bar.set_description(s)
+
+        with torch.no_grad():
+            model.eval()
+            valid_loss = 0
+            print(('\n' + '%10s' * 3) % ('epoch', 'loss', 'gpu'))
+            progress_bar = tqdm.tqdm(enumerate(test_loader), total=len(test_loader))
+
+            for i, (images, targets) in progress_bar:
+                images = images.to(device)
+                targets = targets.to(device)
+                batch_preds, loss = model(images, targets)
+                valid_loss += loss.detach().cpu().item()
+
+                mem = '%.3gG' % (torch.cuda.memory_reserved() / 1E9 if torch.cuda.is_available() else 0)
+                s = ('%10s' + '%10.4g' + '%10s') % ('%g/%g' % (epoch + 1, opt.num_epochs), total_loss / (i + 1), mem)
+                progress_bar.set_description(s)
+
+        scheduler.step(valid_loss)
+
+    torch.save(model.state_dict(), 'weights/final.pth')
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Captcha recognition using RNN')
+    parser.add_argument('--data', type=str, default='samples', help='path to data folder')
+    parser.add_argument('--batch_size', type=int, default=2, help='set the batch size')
+    parser.add_argument('--height', type=int, default=50, help='height of the input image')
+    parser.add_argument('--width', type=int, default=200, help='width of the input image')
+    parser.add_argument('--num_workers', type=int, default=0, help='number of workers in dataloader')
+    parser.add_argument('--num_epochs', type=int, default=300, help='number of epochs')
+    parser.add_argument('--cuda', action='store_true', default=True, help='use cuda')
+    opt = parser.parse_args()
+
+    run_training(opt)
+
+
+
+
+
+
